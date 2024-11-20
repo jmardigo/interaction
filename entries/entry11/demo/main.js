067014bbdbb06bbaba961b84dbf46827bf7473bb
jmardigo <diazj599@newschool.edu>
2024-11-20 03:21:44 -0500

    in class activities!!!

--- entries/entry11/demo/main.js
+++ entries/entry11/demo/main.js
@@ -1,13 +1,12 @@
-/*
-let blobs = document.querySelectorAll(".blob");
-  let container = document.querySelector(".wrapper");
-  // place our blobs randomly
-  blobs.forEach(function (blob) {
-    blob.style.right = (100 * Math.random()) + '%';
-    blob.style.top = (100 * Math.random()) + '%';
-  });
-  container.addEventListener("click", function (event) {
-    if (event.target.classList.contains("blob")) {
-      event.target.remove();
-    }
-  });*/
+
+let button = document.querySelector(".button");
+let colors = ["#255c34", "#726756", "#703a56", "#953e00"];
+let body = document.querySelector("body");
+
+function newColor() {
+  let randIndex = Math.floor(Math.random() * colors.length);
+  let randColor = colors[randIndex];
+  body.style.backgroundColor = randColor;
+}
+
+button.addEventListener("click", newColor);
